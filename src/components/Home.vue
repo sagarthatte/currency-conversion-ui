319d8c2bb76afaa076b1bf96c9e906f931843882
Sagar Thatte <sagarthatte@gmail.com>
2024-04-09 09:50:57 +1000

    Running into token mismatch issues. Partial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <script setup>
-    import Navigation from './Navigation.vue'
+	import { ref, onMounted } from 'vue';
+	import axios from 'axios';
+
+	const user = ref();
+	onMounted(async() => {
+		const data = await axios.get('/api/user');
+		console.log(data);
+	});
 </script>
 
 <template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,26 @@
-<script>
-	export default {
-		data() {
-			return {
-				email: '',
-				password: '',
-				//rememberMe: false,
-			};
-		},
-		methods: {
-			async submitLogin() {
-				console.log('Login');
-				// try {
-				// 	const response = await axios.post(`${process.env.VUE_APP_API_URL}/login`, {
-				// 		email: this.email,
-				// 		password: this.password,
-				// 		remember: this.rememberMe
-				// 	});
+<script setup>
+	import { ref } from 'vue';
+	import axios from 'axios';
+	import { useRouter } from 'vue-router';
 
-				// 	// Handle successful login based on response data (e.g., store token, redirect)
-				// 	console.log('Login successful:', response.data);
-				// } catch (error) {
-				// 	console.error('Login error:', error);
-				// 	// Handle login errors (e.g., display error message)
-				// }
-			},
-		},
-	};
+	const router = useRouter();
+	
+	const form = ref({
+		email: '',
+		password: ''
+	});
+
+	const getToken = async () => {
+		await axios.get('/sanctum/csrf-cookie');
+	}
+	const submitLogin = async() => {
+		await getToken();
+		await axios.post('/login', {
+			email: form.value.email,
+			password: form.value.password
+		});
+		router.push('/');
+	}
 </script>
 
 <template>
@@ -36,30 +31,30 @@
 					<v-card-title class="text-left ml-2">
 						<span class="font-weight-bold">Log In</span>
 					</v-card-title>
-					<v-card-text class="ml-2">
-						<div class="text-left font-weight-medium">Email</div>
-						<v-text-field v-model="email" required type="email" outlined fullWidth />
-						<div class="font-weight-medium text-left">Password</div>
-						<v-text-field v-model="password" required type="password" outlined fullWidth />
-						<!-- <v-checkbox v-model="rememberMe" label="Remember Me" outlined class="ma-0" /> -->
-					</v-card-text>
-					<v-card-actions>
-						<!-- <v-btn block variant="elevated" color="primary" @click="submitLogin" :disabled="!email || !password">Login</v-btn> -->	
-						<router-link :to="{name: 'ResetPassword'}" tag="v-btn">
-							<v-btn color="info" flat class="ml-2">
-								Forgot password?
+					<v-form @submit.prevent="submitLogin">
+						<v-card-text class="ml-2">
+							<div class="text-left font-weight-medium">Email</div>
+							<v-text-field v-model="form.email" required type="email" outlined fullWidth />
+							<div class="font-weight-medium text-left">Password</div>
+							<v-text-field v-model="form.password" required type="password" outlined fullWidth />
+						</v-card-text>
+						<v-card-actions>
+							<router-link :to="{name: 'ResetPassword'}" tag="v-btn">
+								<v-btn color="info" flat class="ml-2">
+									Forgot password?
+								</v-btn>
+							</router-link>
+							<v-spacer></v-spacer>
+							<v-btn variant="tonal" color="#5865f2" type="submit" class="mr-2" :disabled="!form.email || !form.password">
+								Log In
 							</v-btn>
-						</router-link>
-						<v-spacer></v-spacer>
-						<v-btn variant="tonal" color="#5865f2" @click="submitLogin" class="mr-2" :disabled="!email || !password">
-							Log In
-						</v-btn>
-					</v-card-actions>
-					<v-card-actions>
-						<div class="font-weight-medium ml-4" color="info">
-							Not a member yet? <router-link :to="{name: 'Register'}" tag="a"><a href="/register">Sign Up</a></router-link>
-						</div>
-					</v-card-actions>
+						</v-card-actions>
+						<v-card-actions>
+							<div class="font-weight-medium ml-4" color="info">
+								Not a member yet? <router-link :to="{name: 'Register'}" tag="a"><a href="/register">Sign Up</a></router-link>
+							</div>
+						</v-card-actions>
+					</v-form>
 				</v-card>
 			</v-col>
 		</v-row>
