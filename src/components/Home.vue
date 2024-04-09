a957f6f72428c574ca29c20d019a9622f690af61
Sagar Thatte <sagarthatte@gmail.com>
2024-04-09 15:32:09 +1000

    Base working code for home page. Partial commit.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,23 @@
 <script setup>
 	import { ref, onMounted } from 'vue';
 	import axios from 'axios';
+	import ApplicationLogo from './ApplicationLogo.vue';
 
 	const user = ref();
 	onMounted(async() => {
+		// Get token
+		await getToken();
+		// Then get logged in user data
 		const data = await axios.get('/api/user');
 		user.value = data.data;
 	});
+	const getToken = async() => {
+		await axios.get('/sanctum/csrf-cookie');
+	}
 </script>
 
 <template>
+	<ApplicationLogo class="block h-9 w-auto" />
 	<h1>{{ user?.name }}</h1>
 
 	<p>
