776de37432265b8f2146c64db92626dd9b88d452
Sagar Thatte <sagarthatte@gmail.com>
2024-04-09 16:31:09 +1000

    Move state management functionality to pinia store for authentication.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,24 @@
 <script setup>
 	import { ref, onMounted } from 'vue';
-	import axios from 'axios';
+	import { useAuthStore } from '../stores/auth';
 	import ApplicationLogo from './ApplicationLogo.vue';
 
 	const user = ref();
+	const authStore = useAuthStore();
+
 	onMounted(async() => {
-		// Get token
-		await getToken();
-		// Then get logged in user data
-		const data = await axios.get('/api/user');
-		user.value = data.data;
+		await authStore.getUser();
 	});
-	const getToken = async() => {
-		await axios.get('/sanctum/csrf-cookie');
-	}
+
 </script>
 
 <template>
 	<ApplicationLogo class="block h-9 w-auto" />
-	<h1>{{ user?.name }}</h1>
-
-	<p>
-		<h3>{{  user?.email }}</h3>
-	</p>
+	<div v-if="authStore.user">
+		<h3>Hello {{ authStore.user.name }} ({{  authStore.user.email }})</h3>
+	</div>
+	<div v-else>
+		<h3>If you are a new user, please <router-link :to="{name: 'Register'}" tag="a"><a href="/register">Register</a></router-link></h3>
+		<h3>Existing users, please <router-link :to="{name: 'Login'}" tag="a"><a href="/login">Login</a></router-link></h3>
+	</div>
 </template>
